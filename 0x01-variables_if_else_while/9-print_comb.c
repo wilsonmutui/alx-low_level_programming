87a55e35829d58d99a1a607787fdc3e233ebe95a
wilson mutui <patiencechaltu@gmail.com>
2023-09-09 09:56:12 -0700

    my last task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: spacing characters
+ *
+ * Return:  Always return 0 for success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
