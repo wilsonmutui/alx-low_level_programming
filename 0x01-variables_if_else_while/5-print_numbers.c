bc49e87b10f061fb772f58ff84b445293a167548
wilson mutui <patiencechaltu@gmail.com>
2023-09-09 08:58:18 -0700

    base

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description: Single digits
+ *
+ * Return: Always return 0 for sucess
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
