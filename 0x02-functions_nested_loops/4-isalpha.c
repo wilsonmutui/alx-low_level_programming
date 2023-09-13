4c116ec05ee0f46a21229c73c925669c3db506fb
wilson mutui <patiencechaltu@gmail.com>
2023-09-12 22:59:37 -0700

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - Entry point
+ * Description: Returns a value if the character is aphabet
+ * @c: Character to be checked
+ * Return: Retuns 0 and 1 depending of character
+ */
+int _isalpha(int c)
+{
+        if ((c >= 97 && c <= 122) || (c >= 65 && c <= 97))
+        {
+                return (1);
+        }
+        return (0);
+}
