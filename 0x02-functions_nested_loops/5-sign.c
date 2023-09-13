2eb6d5d2dc92dc93824ed1ab79e9de1aac0037eb
wilson mutui <patiencechaltu@gmail.com>
2023-09-12 23:18:09 -0700

    printing signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - Entry point
+ * Description: Prints sign of the letter
+ * Return: Return the sign
+ * @n: Initialised charactr
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(43);
+		return (-1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
