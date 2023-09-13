80380385b9c66bf8d89df5fa23eb87aff96588a6
wilson mutui <patiencechaltu@gmail.com>
2023-09-13 07:44:07 -0700

    print numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Print natural number
+ * @w: Starting interger
+ * Return: Always 0 for succes
+ */
+void print_to_98(int w)
+{
+	if (w <= 98)
+	{
+		for (; w <= 98; w++)
+		{
+			if (w == 98)
+			{
+				printf("%d", w);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", w);
+			}
+		}
+	}
+}
+
