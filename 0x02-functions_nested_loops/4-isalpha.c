62323e4be64fc06c28244c8873f292fc4fb340a2
wilson mutui <patiencechaltu@gmail.com>
2023-09-12 23:06:53 -0700

    corrected

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,9 +7,9 @@
  */
 int _isalpha(int c)
 {
-        if ((c >= 97 && c <= 122) || (c >= 65 && c <= 97))
-        {
-                return (1);
-        }
-        return (0);
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 97))
+	{
+		return (1);
+	}
+	return (0);
 }
