a47eb8b216973bf25feec318c90dd1711c895540
wilson mutui <patiencechaltu@gmail.com>
2023-09-15 01:57:25 -0700

    case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - Prints case valu of an alphabet
+ * @w: Character to be checked
+ * Return: Rerutn 0 or 1 depending on the case
+ */
+int _isupper(int c)
+{
+	if (w >= 'A' && (w <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
