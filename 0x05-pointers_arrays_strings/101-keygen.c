44a845a5ac85f64a2e650642c0f726e72395d733
wilson mutui <patiencechaltu@gmail.com>
2023-09-19 07:49:56 -0700

    password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define pl 12
+
+int main(void)
+{
+    char password[pl +1];
+    const char charset[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    int i;
+
+    srand(time(NULL));
+
+    for (i = 0; i < pl; i++)
+    {
+        int index = rand() % (sizeof(charset) - 1);
+        password[i] = charset[index];
+    }
+
+    password[pl] = '\0';
+
+    printf("%s\n", password);
+
+    return 0;
+}
+
