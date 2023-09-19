da21eb55c159bbf1229f2942046d44ef855a5b4a
wilson mutui <patiencechaltu@gmail.com>
2023-09-19 07:01:29 -0700

    eds

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_strcpy - Coopies string
+ * @dest: String type
+ * @src: String type
+ * Returnn: Pointer to des
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int w = -1;
+
+	do
+	{
+		w++;
+		dest[w] = src[w];
+	}
+	while (*src != '\0');
+
+	return (dest);
+}
