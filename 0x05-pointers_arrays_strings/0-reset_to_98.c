d2bdb6a532ed376ecee10e41b7a19b3078e0f667
wilson mutui <patiencechaltu@gmail.com>
2023-09-19 04:32:28 -0700

    dereferencing

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - A pointer that updates value to 98
+ * @n: Character to be checked
+ * return: Always return 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
