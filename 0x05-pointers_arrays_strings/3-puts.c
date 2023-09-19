5cb39d28ad80349a0b179f068da22cb6885df4ab
wilson mutui <patiencechaltu@gmail.com>
2023-09-19 05:35:14 -0700

    Commit message for all changes

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - Print string to stdout
+ * str: To be printed
+ * return: Returns string
+ */
+void _puts(char *str)
+{
+	int w = 0;
+
+	while (str[w])
+	{
+		_putchar(str[w]);
+		w++;
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - Prints a string in reverse
+ * @s - String to be checked
+ * return: Reversed string
+ */
+void print_rev(char *s)
+{
+	int rev = 0;
+
+	while (s[rev])
+		rev++;
+	while (rev--)
+		_putchar(s[rev]);
+	_putchar('\n');
+}
