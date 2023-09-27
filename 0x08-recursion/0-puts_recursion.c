13469020c60fac81f46da037713cbffb62fb4a00
wilson mutui <patiencechaltu@gmail.com>
2023-09-27 04:41:05 -0700

    prints string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - Prints string followed by new line
+ * @s: input
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar (*s);
+		__puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar ('\n');
+	}
+}
