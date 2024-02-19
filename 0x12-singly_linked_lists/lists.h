b455a4ba4e4b3ff06e7c99d1bf68362ea575e0c9
wilson mutua <patiencechaltu@gmail.com>
2024-02-19 10:48:51 -0800

    singly lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef _LIST_H_
-#define _LIST_H_
+#ifndef LIST_H_
+#define LIST_H_
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,4 +21,9 @@ typedef struct list_s
 	struct list_s *next;
 }
 list_t;
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
 #endif
