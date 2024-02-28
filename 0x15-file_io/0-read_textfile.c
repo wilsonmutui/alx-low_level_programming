abfc3161816f9788829764c9b2fd9d376d65d83d
wilson mutua <patiencechaltu@gmail.com>
2024-02-28 05:53:03 -0800

    printing

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,51 +1,29 @@
 #include "main.h"
 #include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <fcntl.h>  // Include the necessary header for open
-#include <unistd.h>
 
 /**
- * read_textfile - Reads a text file and prints it
- * @filename: name of the file
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
  * @letters: number of letters to be read
- * Return: number of letters
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fileA;
-    ssize_t lenb, lena;
-    char *buffer;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-    if (filename == NULL)  // Fix: Use == for equality comparison
-        return (0);
-    
-    fileA = open(filename, O_RDONLY);
-    if (fileA == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-    {
-        close(fileA);
-        return (0);
-    }
-
-    lenb = read(fileA, buffer, letters);
-    close(fileA);
-
-    if (lenb == -1)  // Fix: Use == for equality comparison
-    {
-        free(buffer);
-        return (0);
-    }
-
-    lena = write(STDOUT_FILENO, buffer, lenb);
-    free(buffer);
-
-    if (lenb != lena)
-        return (0);
-
-    return (lena);
+	free(buf);
+	close(fd);
+	return (w);
 }
 
